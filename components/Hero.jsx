fc9fff6a5dfb644a2b7d67f5ad20089a5fad5b3d
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-24 22:05:39 +0300

    Added Hero Section

---
+++ components/Hero.jsx
@@ -0,0 +1,23 @@
+import {HERO_CONTENT} from "@/index";
+import Image from "next/image"
+
+export default function Hero() {
+    return (
+        <div className="">
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <h1 className="text-8xl tracking-tight pb-16 font-thin ">Milkiyas Gebru</h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                        <p className="my-2 font-light tracking-tighter py-6 max-w-xl">{HERO_CONTENT}</p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <Image src="/Miki_Profile_Picture.jpg" alt="" width={100} height={100} className="w-1/2 rounded"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
