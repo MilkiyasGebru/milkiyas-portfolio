30a9ae64794a76cde204010cfe14100051e2fe9d
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-25 18:49:29 +0300

    Added framer motion to the Hero Section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,24 @@
+"use client"
+
 import {HERO_CONTENT} from "@/constant";
 import Image from "next/image"
+import {motion} from "framer-motion";
+
+const framer_container = (delay,x)=>({
+    hidden: {
+        x:x,
+        opacity: 0,
+    },
+    visible: {
+        x:0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            delay: delay
+        }
+    }
+})
+
 
 export default function Hero() {
     return (
@@ -7,14 +26,29 @@ export default function Hero() {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="text-8xl tracking-tight pb-16 font-thin ">Milkiyas Gebru</h1>
-                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
-                        <p className="my-2 font-light tracking-tighter py-6 max-w-xl">{HERO_CONTENT}</p>
+                        <motion.h1 variants={framer_container(0,-100)}
+                                   initial="hidden"
+                                   animate="visible"
+                                   className="text-8xl tracking-tight pb-16 font-thin ">Milkiyas Gebru</motion.h1>
+                        <motion.span
+                            variants={framer_container(0.25,-100)}
+                            initial="hidden"
+                            animate="visible"
+                            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</motion.span>
+                        <motion.p
+                            variants={framer_container(0.5,-100)}
+                            initial="hidden"
+                            animate="visible"
+                            className="my-2 font-light tracking-tighter py-6 max-w-xl">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <Image src="/Miki_Profile_Picture.jpg" alt="" width={500} height={500} className="max-w-80 rounded"/>
+                        <motion.img
+                            variants={framer_container(0.7, 100)}
+                            initial="hidden"
+                            animate="visible"
+                            src="/Miki_Profile_Picture.jpg" alt="" width={500} height={500} className="max-w-80 rounded"/>
                     </div>
                 </div>
             </div>
