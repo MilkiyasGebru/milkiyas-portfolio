2fc67f6a87f1d25a967f42ed39227888aa5a2442
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-25 17:26:29 +0300

    Added list of images for the project skills

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -6,8 +6,8 @@ export default function Experience() {
             <h1 className="my-20 text-center text-4xl">Experience</h1>
             <div className="flex flex-col gap-8">
                 {EXPERIENCES.map((experience, index) => (
-                    <div key={index} className="flex flex-col items-start md:items-start md:flex-row ">
-                        <div className="w-1/3">
+                    <div key={index} className="flex flex-col items-center md:items-start md:flex-row ">
+                        <div className="w-4/5 md:w-1/3 self-start px-8">
                             <p className="font-thin">{experience.year}</p>
                         </div>
                         <div className="w-4/5 md:w-1/2 flex flex-col gap-y-[10px]">
--- components/Project.jsx
+++ components/Project.jsx
@@ -1,5 +1,18 @@
 import {PROJECTS} from "@/index";
 import Image from "next/image"
+import {
+    SiCss3,
+    SiExpress,
+    SiHtml5,
+    SiJavascript,
+    SiMongodb,
+    SiMysql,
+    SiNumpy,
+    SiPandas,
+    SiReact,
+    SiTailwindcss
+} from "react-icons/si";
+import {BiLogoNodejs, BiLogoPython} from "react-icons/bi";
 
 export default function Project(){
     return (
@@ -14,8 +27,38 @@ export default function Project(){
                             <p className="font-thin">{project.description}</p>
                             <div className="flex gap-2">
                                 {project.technologies.map((technology, index) => (
-                                    <div className="border rounded-md px-3 py-2" key={index}>
-                                        {technology}
+
+                                    <div className="border rounded-md px-3 py-2 text-3xl" key={index}>
+                                        {(() => { // Immediately invoked function expression (IIFE)
+                                            switch (technology.toLowerCase()) {
+                                                case 'html':
+                                                    return <SiHtml5 style={{ color: "#E34F26" }} />;
+                                                case 'css':
+                                                    return <SiCss3 style={{ color: "#1572B6" }} />;
+                                                case 'javascript':
+                                                    return <SiJavascript style={{ color: "#F7DF1E" }} />;
+                                                case 'react':
+                                                    return <SiReact style={{ color: "#61DAFB" }} />;
+                                                case 'node.js':
+                                                    return <BiLogoNodejs style={{ color: "#339933" }} />;
+                                                case 'pandas':
+                                                    return <SiPandas style={{ color: "#150458" }} />;
+                                                case 'numpy':
+                                                    return <SiNumpy style={{ color: "#013243" }} />;
+                                                case 'python':
+                                                    return <BiLogoPython style={{color : "#3776AB"}} />
+                                                case 'express':
+                                                    return <SiExpress style={{ color: "#FFFFFF" }} />
+                                                case 'mysql':
+                                                    return <SiMysql style={{ color: "#4479A1" }} />;
+                                                case 'mongodb':
+                                                    return <SiMongodb style={{ color: "#47A248" }} />;
+                                                case 'tailwindcss':
+                                                    return <SiTailwindcss style={{ color: "#38B2AC" }} />;
+                                                default:
+                                                    return null; // Handle cases where the technology is not recognized
+                                            }
+                                        })()}
                                     </div>
                                 ))}
                             </div>
