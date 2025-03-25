5f520ce3ddd23d3815eebd0fe1832c30a4d2fcdf
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-25 17:33:23 +0300

    Finished the contact page

---
+++ components/Contact.jsx
@@ -0,0 +1,18 @@
+import {CONTACT} from "@/index";
+import Link from "next/link";
+
+export default function Contact() {
+
+    return (
+        <div className="border-b border-neutral-900 pb-10">
+
+            <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+            <div className="text-center tracking-tighter">
+                <p className="mt-4">{CONTACT.address}</p>
+                <p className="mt-4">{CONTACT.phoneNo}</p>
+                <Link href="mailto:milkiyasgebru@gmail.com" className="border-b">{CONTACT.email}</Link>
+            </div>
+
+        </div>
+    )
+}
