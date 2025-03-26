0b1519543dbd911a9f351d6b6af383369b541672
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-26 10:02:41 +0300

    Made the navbar sticky

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="mb-10 flex items-center justify-between py-3 px-3 ">
+        <nav className="mb-10 flex items-center justify-between py-3 px-3 sticky top-0">
             <div className="flex items-center">
                 <p className="font-semibold text-xl">MG</p>
             </div>
