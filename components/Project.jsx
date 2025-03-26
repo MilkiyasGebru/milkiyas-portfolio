4234a0d82891db2194f521b7a90a4e0d85a4f8e2
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-26 12:52:15 +0300

    Fixed the Project Image in small screen

--- components/Project.jsx
+++ components/Project.jsx
@@ -19,12 +19,12 @@ import Link from "next/link";
 
 export default function Project(){
     return (
-        <div className="border-b border-neutral-900 pb-4 container px-4">
+        <div className="border-b border-neutral-900 pb-4 container px-3">
             <h2 className="text-center my-20 text-4xl">Projects</h2>
             <div>
                 {PROJECTS.map((project, index) => (
-                    <div key={index} className="mb-8 flex gap-4 md:gap-16  md:items-start justify-around">
-                        <Image src={project.image} alt="project" width={150} height={150} className={` aspect-square border rounded-md w-1/4 lg:w-1/6  ${index === 2? 'bg-white':""} `} />
+                    <div key={index} className="mb-8 flex gap-4 flex-col items-center md:gap-16 md:flex-row  md:items-start justify-around ">
+                        <Image src={project.image} alt="project" width={150} height={150} className={` aspect-square border rounded-md w-3/5 lg:w-1/6  ${index === 2? 'bg-white':""} `} />
                         <div className="flex flex-col gap-2 items-start w-4/5">
                             <div className="flex gap-x-1 items-center">
                                 <p className="font-semibold">{project.title}</p>
